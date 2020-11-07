f9ef42424244ed05f8bdfc65b879e23a5c495cc9
Rob Williams <rwilliams@spotify.com>
2020-11-07 11:11:35 -0500

    test hard-coding bigger font size

--- src/ui.rs
+++ src/ui.rs
@@ -380,7 +380,7 @@ impl Ui {
     pub fn new() -> Ui {
         let mut font_atlas = FontAtlas::new(
             &include_bytes!("../assets/ProggyClean.ttf")[..],
-            13,
+            20,
             FontAtlas::ascii_character_list(),
         )
         .unwrap();
